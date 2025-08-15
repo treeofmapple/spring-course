25274fa51a336e94965897b44297278951a44827
Samuel Pereira <samuelsantana778@gmail.com>
2025-08-15 09:49:41 -0300

    Large Update

---
+++ frontexample-1/backend-front-example-1/src/main/java/com/tom/sample/example/FrontExample1Application.java
@@ -0,0 +1,13 @@
+package com.tom.sample.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontExample1Application.class, args);
+	}
+
+}
---
+++ frontexample-1/backend-front-example-1/src/test/java/com/tom/sample/example/FrontExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom.sample.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
